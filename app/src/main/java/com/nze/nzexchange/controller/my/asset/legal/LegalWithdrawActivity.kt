cc6cc9e039815648d7640a7bfdd2cb8901151ae0
dell <zhouweiyong55@163.com>
2019-04-21 18:07:55 +0800

    2018/4/21 法币

---
+++ app/src/main/java/com/nze/nzexchange/controller/my/asset/legal/LegalWithdrawActivity.kt
@@ -0,0 +1,40 @@
+package com.nze.nzexchange.controller.my.asset.legal
+
+import android.os.Bundle
+import android.view.View
+import com.nze.nzeframework.netstatus.NetUtils
+import com.nze.nzeframework.tool.EventCenter
+import com.nze.nzexchange.R
+import com.nze.nzexchange.controller.base.NBaseActivity
+
+class LegalWithdrawActivity : NBaseActivity() {
+
+
+    override fun getRootView(): Int = R.layout.activity_legal_withdraw
+
+    override fun initView() {
+    }
+
+    override fun <T> onEventComming(eventCenter: EventCenter<T>) {
+    }
+
+    override fun getOverridePendingTransitionMode(): TransitionMode = TransitionMode.DEFAULT
+
+    override fun isBindEventBusHere(): Boolean = false
+
+    override fun isBindNetworkListener(): Boolean = false
+
+    override fun onNetworkConnected(type: NetUtils.NetType) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onNetworkDisConnected() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getContainerTargetView(): View? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
